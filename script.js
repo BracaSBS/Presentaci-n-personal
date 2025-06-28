de2cd712ac9297798d7481f620e93a91aee04b98
BracaSBS <santiagobracamonte07@gmail.com>
2025-06-28 18:33:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.querySelector(".btn").addEventListener("click", () => {
+    alert("¡Gracias por visitar mi tarjeta de presentación.");
+});
+
+
+document.querySelector(".btn1").addEventListener("click", () => {
+    alert("¡Gracias por visitar mi tarjeta de presentación.");
+});
